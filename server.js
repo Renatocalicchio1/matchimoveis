86b5ead16bcac5692bcfebd398b0b82c384d9be6
Renato Calicchio <renatocalicchio12@yahoo.com>
2026-05-15 16:03:44 -0300

    fix: aumenta limite body para 50mb webhook

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 
 

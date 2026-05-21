7bc9d8321bf1043cbf9923e7927fa28fe7caf35c
Renato Calicchio <renatocalicchio12@yahoo.com>
2026-05-21 00:04:33 -0300

    fix: importXML gera idInterno para cada imovel

--- importXMLCompleto.js
+++ importXMLCompleto.js
@@ -84,6 +84,7 @@ function parseListing(l) {
       id: 'MI-' + Date.now() + '-' + Math.random().toString(36).substr(2,6).toUpperCase(),
       idExterno: idRaw,
       idOriginal: idRaw,
+      idInterno: 'MI-' + Date.now() + '-' + Math.random().toString(36).substr(2,6).toUpperCase(),
       titulo: l.Title || '',
       transacao: l.TransactionType === 'For Sale' ? 'venda' : 'aluguel',
       tipo: normalizeTipo(extractText(details.PropertyType)),
